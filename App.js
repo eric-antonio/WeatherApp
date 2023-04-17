7c0aefd65fe5d7900f455a441b8adc45f69aeebb
Éric Ivan João António <ericantoniodev@gmail.com>
2023-04-17 16:26:37 +0200

    Update App.js

--- App.js
+++ App.js
@@ -5,8 +5,8 @@ const  App = ()=> {
   return (
     <SafeAreaView style={styles.wrapper} >
       <View style={styles.container}>
-       <Text> Current Weather</Text>
-       <Text>6</Text>
+       <Text> Current Weather</Text> 
+       <Text style={styles.temp}>6</Text>
        <Text>Feels like 5</Text>
        <Text>Hing : 8 Low: 6</Text>
       </View>
@@ -22,6 +22,10 @@ const styles = StyleSheet.create({
     backgroundColor: 'pink',
     flex:1,
     alignItems:'center'
+  },
+  temp:{
+    color:'black',
+    fontSize:48,
   }
 
 });
