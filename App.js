cacaf7bd337b1b518e974690ca03cfcb95fc4db5
Éric Ivan João António <ericantoniodev@gmail.com>
2023-04-17 16:02:43 +0200

    Update App.js

--- App.js
+++ App.js
@@ -4,13 +4,16 @@ import {  SafeAreaView,Text, View , StyleSheet} from 'react-native';
 const  App = ()=> {
   return (
     <SafeAreaView>
-      <View style={{backgroundColor: 'pink'}}>
+      <View style={styles.container}>
        <Text>Open up App.js to start working on your app!</Text>
       </View>
     </SafeAreaView>
   );
 }
 const styles = StyleSheet.create({
-  
-})
+  container :{
+    backgroundColor: 'pink'
+  }
+
+});
 export default App;
