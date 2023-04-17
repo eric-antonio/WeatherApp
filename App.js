3bedd4053959ee465d64f1ba925f303b3933805a
Éric Ivan João António <ericantoniodev@gmail.com>
2023-04-17 15:55:55 +0200

    Basic React Native

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {  SafeAreaView,Text, View } from 'react-native';
+
+const  App = ()=> {
+  return (
+    <SafeAreaView>
+      <View style={{backgroundColor: 'pink'}}>
+       <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
