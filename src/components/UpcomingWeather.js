de7f87f6b9c897dd1b3324fe8e9e5f397fc875c4
Éric Ivan João António <ericantoniodev@gmail.com>
2023-04-20 16:43:42 +0200

    Update UpcomingWeather.js

--- src/components/UpcomingWeather.js
+++ src/components/UpcomingWeather.js
@@ -1,11 +1,47 @@
 import React from "react";
 import { SafeAreaView,View ,Text, StyleSheet,FlatList} from "react-native";
 
+const DATA = [
+    {
+        dtx_txt:"2023-02-18 12:00:00",
+        main:{
+            temp_max:8.55,
+            temp_min:7.55
+        },
+        weather:[{
+            main:'Clear'
+        }]
+    },
+    {
+        dtx_txt:"2023-02-18 15:00:00",
+        main:{
+            temp_max:8.55,
+            temp_min:7.55
+        },
+        weather:[{
+            main:'Clouds'
+        }]
+    },
+    {
+        dtx_txt:"2023-02-18 18:00:00",
+        main:{
+            temp_max:8.55,
+            temp_min:7.55
+        },
+        weather:[{
+            main:'Rain'
+        }]
+    }
+]
+
 const UpcomingWeather =()=>{
     return(
         <SafeAreaView style={styles.container}>
             <Text>Upcoming Weather</Text>
-            <FlatList/>
+            <FlatList
+                data={(DATA.at(0))}
+                renderItem={DATA}
+            />
 
         </SafeAreaView>
     )
