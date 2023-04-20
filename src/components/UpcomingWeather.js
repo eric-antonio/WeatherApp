ee5affa10e22a87e2bbae78602d6ad664ab7d04b
Éric Ivan João António <ericantoniodev@gmail.com>
2023-04-20 16:54:11 +0200

    Update UpcomingWeather.js

--- src/components/UpcomingWeather.js
+++ src/components/UpcomingWeather.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { SafeAreaView,View ,Text, StyleSheet,FlatList} from "react-native";
 
+// ? Data ou o nosso array
 const DATA = [
     {
         dtx_txt:"2023-02-18 12:00:00",
@@ -34,12 +35,20 @@ const DATA = [
     }
 ]
 
+// * tempos aqui as props juntamente com o componente  a ideia aqui é fazer a ligação entre o array e o componente (não entendi muito bem , irei tentar ver bem depois na resolução)
+
+// ?Video pausado no tempo : 1:04:35/4:40H
 const Item = (props)=>{
     const { dtx_txt, main , max ,condition }=props
     return(
+        <View>
 
+        </View>
     )
 }
+
+
+//! The component 
 const UpcomingWeather =()=>{
     return(
         <SafeAreaView style={styles.container}>
