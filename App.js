66431f210b3968500fcbb92e7ba630d826ffa4f6
Éric Ivan João António <ericantoniodev@gmail.com>
2023-04-20 09:21:07 +0200

    Update App.js

--- App.js
+++ App.js
@@ -2,12 +2,16 @@ import React from "react";
 import { SafeAreaView, View, Text, StyleSheet} from "react-native";
 const App =()=>{
   return(
+    <View style={styles.container}>
 
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
-
+  container:{
+    flex:1
+  }
 })
 
 export default App;
