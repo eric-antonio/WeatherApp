8d116ce07f989aaedd332056041278a4f00d8dff
Éric Ivan João António <ericantoniodev@gmail.com>
2023-04-24 10:45:24 +0200

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { SafeAreaView, View, Text, StyleSheet} from "react-native";
 import CurrentWeather from "./src/components/CurrentWeather";
 import UpcomingWeather from "./src/components/UpcomingWeather";
-import OurChild from "./src/components/OurChild";
+
 const App =()=>{
   return(
     <View style={styles.container}>
