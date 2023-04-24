a6a72415cfa260d6aa9032e55d327c6f5612e819
Éric Ivan João António <ericantoniodev@gmail.com>
2023-04-24 09:45:03 +0200

    Update OurChild.js

--- src/components/OurChild.js
+++ src/components/OurChild.js
@@ -1,3 +1,12 @@
 import React from "react";
 import { View, Text } from "react-native";
 
+const OurChild = ()=>{
+    return(
+        <View>
+
+        </View>
+    )
+}
+
+export default OurChild;
