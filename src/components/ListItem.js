e3a6764d98e8a0ca7f8696f56ad237d2482d7504
Éric Ivan João António <ericantoniodev@gmail.com>
2023-04-24 14:54:26 +0200

    Update ListItem.js

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -8,14 +8,15 @@ import {Feather}  from '@expo/vector-icons';
 */
 
 const ListItem = (props)=>{
-    const { dt_txt, min , max ,condition }=props
+    const { dt_txt, min , max ,condition }= props
+    const {date, temp, item}= styles
     return(
-        <View style={styles.item}>
+        <View style={item}>
             
             <Feather name={'sun'} size={50} color={'#fffafa'}/>
-            <Text style={styles.date}>{dt_txt}</Text>
-            <Text style={styles.temp}>{min}</Text>
-            <Text style={styles.temp}>{max}</Text>
+            <Text style={date}>{dt_txt}</Text>
+            <Text style={temp}>{min}</Text>
+            <Text style={temp}>{max}</Text>
 
         </View>
     )
