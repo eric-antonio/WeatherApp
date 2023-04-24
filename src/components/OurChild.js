d9a228fe497f34cbb388c57093eac0932cb88d23
Éric Ivan João António <ericantoniodev@gmail.com>
2023-04-24 10:43:23 +0200

    Update OurChild.js

--- src/components/OurChild.js
+++ src/components/OurChild.js
@@ -1,14 +1,22 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text , StyleSheet} from "react-native";
 
 const OurChild = (props) => {
     const {message} =  props
     return(
-        <View style={{height:200, width:200 , backgroundColor:'pink'}}>
+        <View style={styles.container}>
             <Text>{message}</Text>
 
         </View>
     )
 }
-
+const styles = StyleSheet.create({
+    container:{
+        height:200,
+        width:200 ,
+        backgroundColor:'pink',
+        justifyContent: 'center',
+        alignItems:'center'
+    }
+})
 export default OurChild;
