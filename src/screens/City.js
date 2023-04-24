b28268ee097c631ccdef5711d87761f5dd753367
Éric Ivan João António <ericantoniodev@gmail.com>
2023-04-24 15:11:21 +0200

    Update City.js

--- src/screens/City.js
+++ src/screens/City.js
@@ -3,17 +3,20 @@ import { SafeAreaView ,View, Text , StyleSheet} from "react-native";
 
 //* A nossa Tela.
 const City = () =>{
-    const {} = styles
+    const {container} = styles
     return(
-        <SafeAreaView>
-
+        <SafeAreaView style={container}>
+            <Text>City</Text>
         </SafeAreaView>
     )
 }
 
 //* O nosso CSS
 const styles = StyleSheet.create({
-
+    container:{
+        flex: 1
+      
+    }
 })
 
 export default City;
