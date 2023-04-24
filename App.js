48e12fc087ee1dc7e7455e2c15d01170fdfe4bf6
Éric Ivan João António <ericantoniodev@gmail.com>
2023-04-24 15:11:25 +0200

    Update App.js

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import React from "react";
 import { SafeAreaView, View, Text, StyleSheet} from "react-native";
 import CurrentWeather from "./src/screens/CurrentWeather";
 import UpcomingWeather from "./src/screens/UpcomingWeather";
+import City from "./src/screens/City";
 
 const App =()=>{
   return(
     <View style={styles.container}>
-      <UpcomingWeather/>
+      <City/>
     </View>
   )
 }
