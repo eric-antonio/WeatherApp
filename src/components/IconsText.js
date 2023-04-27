b79a07e1027e72216dc1721adfad10202d819ab8
Éric Ivan João António <ericantoniodev@gmail.com>
2023-04-27 22:00:17 +0200

    Update IconsText.js

--- src/components/IconsText.js
+++ src/components/IconsText.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { SafeAreaView,View, Text, StyleSheet  } from "react-native";
+import { Feather } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
 const IconsText = () =>{
     return(
         <SafeAreaView>
