936d53640085476f351437cb6844a9bf380f99b9
Éric Ivan João António <ericantoniodev@gmail.com>
2023-04-27 21:57:45 +0200

    Create IconsText.js

---
+++ src/components/IconsText.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { SafeAreaView } from "react-native";
+const IconsText = () =>{
+    return(
+        <SafeAreaView>
+
+        </SafeAreaView>
+    )
+}
+
+export default IconsText;
