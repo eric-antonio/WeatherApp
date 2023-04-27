1147454deb86a7c449b44ab34c4cd4886c7153d5
Éric Ivan João António <ericantoniodev@gmail.com>
2023-04-27 15:01:22 +0200

    Update City.js

--- src/screens/City.js
+++ src/screens/City.js
@@ -1,30 +1,28 @@
 import React from "react";
-import { SafeAreaView, View, Text, ImageBackground, StyleSheet } from "react-native/types";
+import { SafeAreaView ,View, Text , StyleSheet, ImageBackground, StatusBar} from "react-native";
+import { AntDesign } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
 
-const City =()=>{
-    const{ container,
-        imageLayout,
-        countryName, 
-        cityName, 
-        cityText, 
-        populationWrapper, 
-        populationText,
-        riseSetWrapper,
-        riseSetText
-    } = styles
+//* A nossa Tela.
+const City = () =>{
+    const{ container} = styles
     return(
         <SafeAreaView style={container}>
-
+            {/* Imagem de fundo com componenetes por dentro.  */}
+            <Text>Hello</Text>
 
         </SafeAreaView>
     )
 }
 
-const styles =  StyleSheet.create({
+const styles = StyleSheet.create({
     container:{
         flex: 1,
         marginTop: StatusBar.currentHeight || 0,
+        justifyContent: 'center',
+        alignItems:'center',
+        backgroundColor: 'pink'
     }
-
 })
+
 export default City;
