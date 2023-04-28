3e1142aa54d23e67fa4e201219b2bdb4a0982fa5
Éric Ivan João António <ericantoniodev@gmail.com>
2023-04-28 20:37:49 +0200

    Update IconText.js

--- src/components/IconText.js
+++ src/components/IconText.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { SafeAreaView,View, Text, StyleSheet  } from "react-native";
 import { Feather } from '@expo/vector-icons';
-import { AntDesign } from '@expo/vector-icons';
+
 
 const IconsText = (props) =>{
     const {iconName , iconColor ,  bodyText, bodyTextStyles} = props
@@ -9,7 +9,7 @@ const IconsText = (props) =>{
     return(
         <View>
             
-            <AntDesign name={iconName} size={50} color={iconColor} />
+            <Feather name={iconName} size={50} color={iconColor} />
 
             <Text style={ [ styles.textTheme, bodyTextStyles ] }>{bodyText}</Text>
 
