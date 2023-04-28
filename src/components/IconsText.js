5812725877684add08fe2e666f11129f2f5be88c
Éric Ivan João António <ericantoniodev@gmail.com>
2023-04-28 19:25:27 +0200

    Update IconsText.js

--- src/components/IconsText.js
+++ src/components/IconsText.js
@@ -5,18 +5,22 @@ import { AntDesign } from '@expo/vector-icons';
 
 const IconsText = (props) =>{
     const {iconName , iconColor ,  bodyText} = props
+    const {textTheme}  = styles
     return(
         <View>
             
             <AntDesign name={iconName} size={50} color={iconColor} />
 
-            <Text style={populationText}>{bodyText}</Text>
+            <Text style={textTheme}>{bodyText}</Text>
 
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    textTheme:{
+        
+    }
 
 })
 
