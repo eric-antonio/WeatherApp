95c0b1d1426f81a76f3abf438862e6c64a71c13e
Éric Ivan João António <ericantoniodev@gmail.com>
2023-04-28 19:30:42 +0200

    Update City.js

--- src/screens/City.js
+++ src/screens/City.js
@@ -2,7 +2,7 @@ import React from "react";
 import { SafeAreaView ,View, Text , StyleSheet, ImageBackground, StatusBar} from "react-native";
 import { Feather } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
-import IconsText from "../components/iconsText";
+import IconsText from "../components/IconText";
 
 
 const City = () =>{
