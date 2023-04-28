44c61c77796d13e9aee4fba6b334a8c85feff68a
Éric Ivan João António <ericantoniodev@gmail.com>
2023-04-28 19:31:13 +0200

    Update City.js

--- src/screens/City.js
+++ src/screens/City.js
@@ -2,7 +2,7 @@ import React from "react";
 import { SafeAreaView ,View, Text , StyleSheet, ImageBackground, StatusBar} from "react-native";
 import { Feather } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
-import IconsText from "../components/IconText";
+import IconText from "../components/IconText";
 
 
 const City = () =>{
@@ -28,7 +28,7 @@ const City = () =>{
 
                 {/* Poupulation View */}
                 <View style={populationWrapper}>
-                    <IconsText/>
+                    <IconText/>
                 </View>
 
                 {/* * Sun set & Sun Rise */}
