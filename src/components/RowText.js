69acf63e67461d30813650dc5500f087204e608d
Éric Ivan João António <ericantoniodev@gmail.com>
2023-04-29 09:06:22 +0200

    Update RowText.js

--- src/components/RowText.js
+++ src/components/RowText.js
@@ -2,13 +2,13 @@ import React from "react";
 import { View, Text , StyleSheet} from "react-native";
 
 const RowText = (props)=> {
-    const {higLow ,  bodyText} =  props
+    const {higLow ,  messageOne, messageToo} =  props
     const {higLowWrapper} = styles
     return(
         <View style={styles.higLowWrapper}>
 
-          <Text style={styles.higLow}>{bodyText}</Text>
-          <Text style={styles.higLow}>{bodyText} </Text>
+          <Text style={styles.higLow}>{messageOne}</Text>
+          <Text style={styles.higLow}>{messageToo} </Text>
 
         </View>
     )
