d51e9aca747b9a51779d62dfdbfeabaa586541aa
Éric Ivan João António <ericantoniodev@gmail.com>
2023-05-02 08:43:11 +0200

    Update App.js

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { SafeAreaView, View, Text, StyleSheet ,StatusBar} from "react-native";
 import CurrentWeather from "./src/screens/CurrentWeather";
 import UpcomingWeather from "./src/screens/UpcomingWeather";
 import City from "./src/screens/City";
+import { NavigationContainer } from "@react-navigation/native";
 
 const App =()=>{
   return(
