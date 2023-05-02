923eb8464f9a705b5d8ca55f3a170327ea85c8f4
Éric Ivan João António <ericantoniodev@gmail.com>
2023-05-02 08:21:21 +0200

    Update RowText.js

--- src/components/RowText.js
+++ src/components/RowText.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text , StyleSheet} from "react-native";
+import { View, Text } from "react-native";
 
 const RowText = (props)=> {
     
@@ -20,9 +20,4 @@ const RowText = (props)=> {
         </View>
     )
 }
-
-const styles =  StyleSheet.create({
-
-})
-
 export default RowText;
