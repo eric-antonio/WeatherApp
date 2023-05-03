d19f3963262960e1ee21a645944144306b75f327
Éric Ivan João António <ericantoniodev@gmail.com>
2023-05-03 07:03:24 +0200

    Update App.js

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ const App =()=>{
 
         <Tab.Screen name={'Current'} component={CurrentWeather}/>
         <Tab.Screen name={'Upcoming'} component={UpcomingWeather}/>
-        
+        <Tab.Screen name= {'City'} component={City}/>
 
       </Tab.Navigator>
 
