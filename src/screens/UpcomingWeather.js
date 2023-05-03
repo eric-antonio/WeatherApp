e961a3ef4e3d7c4f254df8c234176b816ae6a262
Éric Ivan João António <ericantoniodev@gmail.com>
2023-05-03 07:07:27 +0200

    Update UpcomingWeather.js

--- src/screens/UpcomingWeather.js
+++ src/screens/UpcomingWeather.js
@@ -93,7 +93,7 @@ const UpcomingWeather =()=>{
 const styles = StyleSheet.create({
     container:{
         flex:1,
-        marginTop: StatusBar.currentHeight || 0,
+        // marginTop: StatusBar.currentHeight || 0,
         backgroundColor:'#00bfff'
     },
     title:{
