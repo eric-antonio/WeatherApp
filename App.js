2b330d9d8e34620814ddf704c6edf2285a1ae2ea
Éric Ivan João António <ericantoniodev@gmail.com>
2023-05-03 07:27:23 +0200

    Update App.js

--- App.js
+++ App.js
@@ -2,22 +2,20 @@ import React from "react";
 import CurrentWeather from "./src/screens/CurrentWeather";
 import UpcomingWeather from "./src/screens/UpcomingWeather";
 import City from "./src/screens/City";
-import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 
 const App =()=>{
   const Tab = createBottomTabNavigator()
-  const {nameText} = styles
   return(
     <NavigationContainer>
 
       <Tab.Navigator>
 
-        <Tab.Screen name={'Current'} component={CurrentWeather} style={nameText}/>
-        <Tab.Screen name={'Upcoming'} component={UpcomingWeather} style={nameText}/>
-        <Tab.Screen name= {'City'} component={City} style={nameText}/>
+        <Tab.Screen name={'Current'} component={CurrentWeather}/>
+        <Tab.Screen name={'Upcoming'} component={UpcomingWeather}/>
+        <Tab.Screen name= {'City'} component={City}/>
 
       </Tab.Navigator>
 
@@ -25,11 +23,5 @@ const App =()=>{
 
   )
 }
-
-const styles = StyleSheet.create({
-  nameText:{
-    textAlign
-  }
-})
 
 export default App;
