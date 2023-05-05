57cac169b665d3ab686b7dbd0cba6c78071a8f32
Éric Ivan João António <ericantoniodev@gmail.com>
2023-05-05 21:51:26 +0200

    New roots for the screens

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,7 +1,7 @@
 import React from "react";
-import CurrentWeather from "./src/screens/CurrentWeather";
-import UpcomingWeather from "./src/screens/UpcomingWeather";
-import City from "./src/screens/City";
+import CurrentWeather from "../screens/CurrentWeather";
+import UpcomingWeather from "../screens/UpcomingWeather";
+import City from "../screens/City";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Feather } from '@expo/vector-icons';
