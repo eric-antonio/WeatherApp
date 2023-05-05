82fa671cb7d72b895c201fef3872c03871845f34
Éric Ivan João António <ericantoniodev@gmail.com>
2023-05-05 21:48:55 +0200

    Passing the The Navigator here
    Now we have this component that contains those Tabs !

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -6,3 +6,60 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Feather } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
+
+const Tabs =()=>{
+    return(
+        <Tab.Navigator screenOptions={{tabBarActiveTintColor:'#00bfff',tabBarInactiveTintColor:'grey '}}>
+
+        {/* Icon Current  */}
+        <Tab.Screen 
+          name={'Current'} 
+          component={CurrentWeather}
+          options={{
+            tabBarIcon: ({focused}) =>(
+              <Feather
+                name={'droplet'}
+                size={25}
+                color={focused ? '#00bfff' : 'black'}
+              />
+            )
+          }}
+        
+        />
+        {/* Icon Upcoming */}
+        <Tab.Screen 
+          name={'Upcoming'} 
+          component={UpcomingWeather}
+          options={{
+            tabBarIcon:({focused}) =>(
+              <Feather
+                name={'clock'}
+                size={25}
+                color={focused ? '#00bfff' : 'black'}
+              />
+            )
+          }}
+        />
+
+
+        {/* Icon City */}
+        <Tab.Screen 
+          name= {'City'} 
+          component={City}
+          options={{
+            tabBarIcon:({focused}) =>(
+              <AntDesign
+                name={'home'}
+                size={25}
+                color={focused ? '#00bfff' : 'black'}
+              />
+            )
+          }}
+        
+        />
+
+      </Tab.Navigator>
+    )
+}
+
+export default Tabs;
