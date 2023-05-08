0f61a1917f41014693b89467973b916c5a80ae71
Éric Ivan João António <ericantoniodev@gmail.com>
2023-05-08 20:38:15 +0200

    Update Counter.js

--- src/demonstration/Counter.js
+++ src/demonstration/Counter.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { View , Text , Button , StyleSheet } from "react-native";
 const Counter =()=>{
+    let count = 0
     return(
         <View>
-
+            <Text>{`Count: ${count}`}</Text>
+            <Button/>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    
+
 })
 export default  Counter;
