db819c09a866b8609822475fd4b8f0978bb8878a
Éric Ivan João António <ericantoniodev@gmail.com>
2023-05-11 09:16:21 +0200

    Back to weather app

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./src/components/Tabs";
-import Counter from "./src/demonstration/Counter";
+
 
 const App =()=>{
   return(
     <NavigationContainer>
-      {/* <Tabs/> */}
-      <Counter/>
+      <Tabs/>
+     
     </NavigationContainer>
 
   )
