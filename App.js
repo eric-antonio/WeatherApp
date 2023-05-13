519497ce41033a0dcad68d7e870bab48eb08d985
Éric  António <ericantoniodev@gmail.com>
2023-05-13 20:46:19 +0200

    setting activityIndicator

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React, {useState} from "react";
-import { ActivityIndicator } from "react-native/types";
+import { ActivityIndicator, View } from "react-native/types";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./src/components/Tabs";
 
 
 const App =()=>{
   const [loading , setLoading ]=  useState(true)
+  if(loading){
+    return(
+      <View>
+        <ActivityIndicator/>
+      </View>
+    )
+  }
+
   return(
     <NavigationContainer>
       <Tabs/>
