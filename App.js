0668794049f6d62ed0f66d55951658e155fa7974
Éric  António <ericantoniodev@gmail.com>
2023-05-14 09:44:28 +0200

    Can get Location!
    The app asked permission to get location!

--- App.js
+++ App.js
@@ -2,13 +2,34 @@ import React, {useState, useEffect} from "react";
 import {ActivityIndicator, View ,StyleSheet} from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./src/components/Tabs";
-
+import * as Location from 'expo-location';
 
 const App =()=>{
 
   const [loading , setLoading] = useState(true)
-  const [location, setLocation] =  useState(null)
-  const [errorMsg, setErrorMsg] = useState(null)
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
 
   //* Activity Indicator Loading
   if(loading){
@@ -18,10 +39,6 @@ const App =()=>{
       </View>
     )
   }
-  // * Catching Location from Expo-Location
-  useEffect(()=>{
-
-  })
 
   // ? App returnig the Tabs Navigation!
   return(
