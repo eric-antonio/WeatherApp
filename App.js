7fcf7808cb9ab08e5d3c925c318a09e277369e7d
Éric  António <ericantoniodev@gmail.com>
2023-05-14 08:42:07 +0200

    Initializing Dynamic !

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
 import React, {useState} from "react";
-import {View , ActivityIndicator ,StyleSheet} from "react-native";
+import {ActivityIndicator } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./src/components/Tabs";
 
 
 const App =()=>{
-
-  const [loading , setLoading ] =  useState(true)
-
-  if(loading){
-    return(
-      <View style={styles.container}>
-        <ActivityIndicator size={'large'} color={'blue'}/>
-      </View>
-    )
-  }
 
   return(
     <NavigationContainer>
@@ -25,11 +15,6 @@ const App =()=>{
 
   )
 }
-const styles = StyleSheet.create({
-  container:{
-    justifyContent:'center',
-    flex:'1'
-  }
-})
+
 
 export default App;
