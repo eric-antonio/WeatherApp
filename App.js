e14d217229633ea5a9a6c0254a05b0dd2c99d9d3
Éric  António <ericantoniodev@gmail.com>
2023-05-14 09:10:03 +0200

    Expo Location
    Steps:
          * imported location
           *  Set some comments to clear the code.
            *  useEfect() applied

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import {ActivityIndicator, View ,StyleSheet} from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./src/components/Tabs";
@@ -7,6 +7,10 @@ import Tabs from "./src/components/Tabs";
 const App =()=>{
 
   const [loading , setLoading] = useState(true)
+  const [location, setLocation] =  useState(null)
+  const [errorMsg, setErrorMsg] = useState(null)
+
+  //* Activity Indicator Loading
   if(loading){
     return(
       <View style={styles.container}>
@@ -14,6 +18,12 @@ const App =()=>{
       </View>
     )
   }
+  // * Catching Location from Expo-Location
+  useEffect(()=>{
+
+  })
+
+  // ? App returnig the Tabs Navigation!
   return(
     <NavigationContainer>
 
