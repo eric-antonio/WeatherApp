3569635423a70a8338c0b6b2be49eaa7b4766c7e
Éric  António <ericantoniodev@gmail.com>
2023-05-22 20:58:48 +0200

    Update CurrentWeather.js

--- src/screens/CurrentWeather.js
+++ src/screens/CurrentWeather.js
@@ -5,16 +5,6 @@ import RowText from '../components/RowText';
 import { weatherType } from '../utilities/weatherType';
 const  CurrentWeather = (weatherData)=> {
 
-  const {
-    container,
-    tempStyles,
-    feels,
-    higLowWrapper,
-    higLow,
-    bodyWrapper,
-    description,
-    message
-  } = styles;
 
   const {
     main:{temp, feels_like, temp_max, temp_min}, 
