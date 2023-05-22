5edfc0ad42cf3e37348cd89959da4e7975cce543
Éric  António <ericantoniodev@gmail.com>
2023-05-22 20:59:19 +0200

    Update weatherType.js

--- src/utilities/weatherType.js
+++ src/utilities/weatherType.js
@@ -4,40 +4,4 @@ export const weatherType ={
         message:'It could get noisy!',
         background:'#000000',
     },
-    Drizzle:{
-        icon:'cloud-rain',
-        message:'It might rain a little!',
-        background:'#3645f',
-    },
-    Rain:{
-        icon:'umbrella',
-        message:'You will need an Umbrella!',
-        background:'#0000ff',
-    },
-    Snow:{
-        icon:'cloud-snow',
-        message:'Lets build a snowman!',
-        background:'#7f6065',
-    },
-    Clear:{
-        icon:'sun',
-        message:'It is perfect t-shirt weather!',
-        background:'#e47200',
-    },
-    Clouds:{
-        icon:'cloud',
-        message:'You could leave in the clouds!',
-        background:'#363636',
-    },
-    Haze:{
-        icon:'wind',
-        message:'It might be a bit damp!',
-        background:'#58586e',
-    },
-    Mist:{
-        icon:'align-justify',
-        message:'It might be hard to see!',
-        background:'#3e3e37',
-        temp:''
-    },
-}
+    Drizzle
