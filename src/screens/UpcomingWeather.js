d434086bff38f066548c79597582d6b34a44c5fa
Éric  António <ericantoniodev@gmail.com>
2023-05-31 22:05:21 +0200

    Update UpcomingWeather.js

--- src/screens/UpcomingWeather.js
+++ src/screens/UpcomingWeather.js
@@ -9,14 +9,6 @@ import { SafeAreaView,
 import {Feather} from '@expo/vector-icons';
 import ListItem from "../components/ListItem";
 
-// ? Data ou o nosso array.
-const DATA = [
-    {
-        dt_txt:"2023-02-18 12:00:00",
-        main:{
-            temp_max:8.55,
-            temp_min:7.55
-        },
         weather:[{
             main:'Clear'
         }]
